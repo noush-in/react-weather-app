c0f8cec2c6ae90cc68601f862bac5c06541c0895
noush-in <noushinheidari1990@gmail.com>
2022-03-02 22:16:57 +0330

    bootstrap added but not working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-<h1>Hello world</h1>
-    </div>
-  );
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+	return (
+		<div className="App">
+			<div className="container">
+				<h1>Weather App</h1>
+				<button type="button" className="btn btn-primary">
+					buttn
+				</button>
+				<footer>
+					This project is coded by{" "}
+					<a href="https://github.com/noush-in" target="_blank">
+						Noushin Heidari
+					</a>{" "}
+					and it's{" "}
+					<a
+						href="https://github.com/noush-in/react-weather-app"
+						target="_blank">
+						Open sourced on github
+					</a>
+					<div class="card">
+						<div class="card-body">This is some text within a card body.</div>
+					</div>
+				</footer>
+			</div>
+		</div>
+	);
 }
-
-export default App;
