2ca3cc37a85e278d608d6541714e146a7b802a64
noush-in <noushinheidari1990@gmail.com>
2022-03-11 19:05:23 +0330

    WeatherForecastDDay is added

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,31 @@
+/** @format */
+
+import React from "react";
+
+import WeatherIcon from "./WeatherIcon";
+
+
+export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+	return (
+		<div className="WeatherForecast">
+			<div className="WeatherForecast-day">{day()}</div>
+			<div className="WeatherForecast-icon">
+				<WeatherIcon code={props.data.weather[0].icon} size={36} />
+			</div>
+			<div className="WeatherForecast-temperature">
+				<span className="WeatherForecast-temperature-max">
+					{Math.round(props.data.temp.max)}°
+				</span>
+				<span className="WeatherForecast-temperature-min">
+					{Math.round(props.data.temp.min)}°
+				</span>
+			</div>
+		</div>
+	);
+}
