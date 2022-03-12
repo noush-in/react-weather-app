c0cf57793c021d828667e45a690470514cdc7237
noush-in <noushinheidari1990@gmail.com>
2022-03-12 11:10:27 +0330

    finishing refresh daily forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState ,useEffect } from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -13,6 +13,11 @@ export default function WeatherForecast(props) {
 		setLoaded(true);
 	}
 
+
+	useEffect(() => {
+		setLoaded(false);
+	}, [props.coordinates]);
+
 	if (loaded) {
 		return (
 			<div className="row">
@@ -23,6 +28,8 @@ export default function WeatherForecast(props) {
 								<WeatherForecastDay data={dailyForecast} />
 							</div>
 						);
+					}else{
+						return null;
 					}
 				})}
 			</div>
